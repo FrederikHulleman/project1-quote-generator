fcc94ee232b301a750c3fb6c28e13a2ab9c8beb8
Frederik Hulleman <fhulleman@gmail.com>
2019-09-16 12:19:11 +0200

    Added auto refresh

--- index.php
+++ index.php
@@ -24,6 +24,8 @@ $colors = generateColors();
   <html lang="en">
   <head>
     <meta charset="UTF-8">
+    <!-- Redirect page after 3 seconds -->
+    <meta http-equiv="refresh" content="10;url=/project1-quote-generator/">
     <title>Random Quotes</title>
     <link href='https://fonts.googleapis.com/css?family=Playfair+Display:400,400italic,700,700italic' rel='stylesheet' type='text/css'>
     <link rel="stylesheet" href="css/normalize.css">
